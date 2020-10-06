4f12a659f7bba6b3a613626c839376d414e662f3
dvd42 <diegovd0296@gmail.com>
2020-10-06 15:56:17 +0200

    fixed visualization

--- scripts/eval_coco.sh
+++
@@ -1,4 +0,0 @@
-python -m torch.distributed.launch --nproc_per_node=1 --use_env main.py --no_aux_loss \
-    --data_path datasets/data/coco --dataset_file coco --batch_size 2 \
-    --resume https://dl.fbaipublicfiles.com/detr/detr-r50-e632da11.pth --eval \
-    --pyramid 3 2 --output_dir output/coco/eval
